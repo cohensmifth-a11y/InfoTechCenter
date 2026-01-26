6a79e4756b854c7c6e4f6ef910a7a2e056f1edd5
Smith <cohensmifth@gmail.com>
2026-01-26 13:49:02 -0500

    Weather Function - Calls random weather

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,1 +1,20 @@
 #Weather Branch
+
+import random
+
+weather_events = [
+    "Sunny",
+    "Rain",
+    "Heavy Rain",
+    "Snow",
+    "Blizzard",
+    "Hail",
+    "Ice Rain",
+    "Thunderstorm",
+    "Fog",
+    "Windstorm"
+]
+
+random_weather = random.choice(weather_events)
+
+print("Today's weather event:", random_weather)
